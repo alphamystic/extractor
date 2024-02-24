ac37e76c7cfa401af35502fcb52051817aa4b4a5
sam <sam@sam.sam>
2024-02-24 16:43:15 +0300

    Removed working code now I need to get it back, well there's anproblem with the appending

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Extractor is a simple golang engine to extract files from a pcap.
 
 This script should not be used for a production setup as it might slow things down. Try with EBPF or something else at roouter level(Like firewalls do)
+
+
+
+The whole point is to be able to run a binary or click a link,open a pdf/word document then follow a TA from initial access to ransomware or exfill from a pcap before going ahead to RE given binaries. An endgoal to have this for IR n generating artifacts.
--- extractor.go
+++ extractor.go
@@ -4,6 +4,7 @@ import (
   "io"
   "os"
   "fmt"
+  "sync"
   "bytes"
   //"strings"
   "github.com/google/gopacket"
@@ -24,6 +25,7 @@ type streamFactory struct{}
 type PStream struct {
   net, transport gopacket.Flow
   r tcpreader.ReaderStream
+  fileLock sync.Mutex
 }
 
 func (f *streamFactory) New(net, transport gopacket.Flow) tcpassembly.Stream {
@@ -38,112 +40,69 @@ func (f *streamFactory) New(net, transport gopacket.Flow) tcpassembly.Stream {
 
 // fileTypeSignatures holds the byte signatures of different file types for detection.
 var fileTypeSignatures = map[string][]byte{
-    "exe": []byte{0x4D, 0x5A},
-    "zip": []byte{0x50, 0x4B, 0x03, 0x04},
-    "png": []byte{0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A},
+  "exe": []byte{0x4D, 0x5A},
+  //"zip": []byte{0x50, 0x4B, 0x03, 0x04},
+  //"png": []byte{0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A},
 }
 
-/*
+func saveFile(data []byte, fileTypeDetected string,lock *sync.Mutex) {
+  lock.Lock() // Lock before writing to the file
+  defer lock.Unlock() // Ensure the lock is released after writing
+	// This is a simplified example. You should add error checking and handling.
+	tempFileName := "temp_file"
+	finalFileName := fmt.Sprintf("%s.%s", tempFileName, fileTypeDetected)
+  file, err := os.OpenFile(finalFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("[-] Error opening file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	if _, err := file.Write(data); err != nil {
+		fmt.Printf("[-] Error writing to file: %v\n", err)
+	}
+
+	/*os.WriteFile(tempFileName, data, 0644) // Write the data to a temporary file
+	os.Rename(tempFileName, finalFileName) // Rename the file based on detected file type
+  */
+	fmt.Printf("File saved as %s\n", finalFileName)
+}
+
 func (ps *PStream) run() {
-  endTmp := &bytes.Buffer{} // Final buffer to accumulate all stream data.
-  tmp := make([]byte, 4096) // Temp buffer for reading stream data.
-  var fileTypeDetected string
+  tmp := make([]byte, 4096) // Temp buffer for initial read and detection
+	n, err := ps.r.Read(tmp)
+	if err != nil && err != io.EOF {
+		fmt.Printf("[-] Error reading stream: %v\n", err)
+		return
+	}
+	// Attempt to detect file type
+	var fileTypeDetected string
+	data := tmp[:n]
+	for fileType, signature := range fileTypeSignatures {
+		if bytes.Contains(data, signature) {
+			fileTypeDetected = fileType
+			fmt.Println("[+] Detected file type:", fileType)
+			break
+		}
+	}
+  // write the data to file then start stream reading to EOF
+  saveFile(data,fileTypeDetected,&ps.fileLock)
+  fmt.Println("Done with the first reading......")
   for {
-    n, err := ps.r.Read(tmp)
-    if n > 0 {
-      data := tmp[:n]
-      endTmp.Write(data) // Append to final buffer regardless of file type detection status.
-      if fileTypeDetected == "" {
-        // Attempt to detect the file type if not already detected.
-        for fileType, signature := range fileTypeSignatures {
-          if bytes.Contains(endTmp.Bytes(), signature) {
-            fileTypeDetected = fileType
-            fmt.Println("[+] Detected file type:", fileType)
-            // No break here to ensure all data is read; fileTypeDetected prevents re-checking.
-          }
-        }
-      }
-    }
-    if err == io.EOF {
-      break // End of stream.
-    } else if err != nil {
-      fmt.Printf("[-] Error reading stream: %v\n", err)
-      return
-    }
-  }
-  if fileTypeDetected != "" {
-    // Save the detected file to disk.
-    saveFile(endTmp.Bytes(), fileTypeDetected)
-  }
-}
-*/
-
-func (ps *PStream) run() {
-  endTmp := &bytes.Buffer{} // Buffer to accumulate stream data
-    tmp := make([]byte, 4096) // Temp buffer for reading stream data
-    var fileTypeDetected string
-
-    for {
-        n, err := ps.r.Read(tmp)
-        if n > 0 {
-            data := tmp[:n]
-            // Append read data to the buffer
-            endTmp.Write(data)
-
-            // If file type hasn't been detected yet, attempt to detect it
-            if fileTypeDetected == "" {
-                for fileType, signature := range fileTypeSignatures {
-                    if bytes.Contains(endTmp.Bytes(), signature) {
-                        fileTypeDetected = fileType
-                        fmt.Println("[+] Detected file type:", fileType)
-                        // Once file type is detected, no need to check again
-                        break
-                    }
-                }
-            }
-        }
-
-        // If EOF, exit loop
-        if err == io.EOF {
-            break
-        } else if err != nil {
-            fmt.Printf("[-] Error reading stream: %v\n", err)
-            return
-        }
-    }
-
-    // After reading the entire stream, if a file type was detected, save the data
-    if fileTypeDetected != "" {
-        saveFile(endTmp.Bytes(), fileTypeDetected)
-    }
-}
-
-func saveFile(data []byte, fileType string) {
-    // Generate a unique filename for each saved file.
-    exeCounter++
-    filename := fmt.Sprintf("extracted_%d.%s", exeCounter, fileType)
-    err := os.WriteFile(filename, data, 0644)
-    if err != nil {
-        fmt.Printf("[-] Failed to save file %s: %v\n", filename, err)
-        return
-    }
-    fmt.Printf("[+] Saved %s successfully.\n", filename)
-}
-
-
-func writePayloadToFile(payload []byte, filename string) {
-  file, err := os.Create(filename)
-  if err != nil {
-    fmt.Printf("[-] Failed to create file: %v\n", err)
-    return
-  }
-  defer file.Close()
-  _, err = file.Write(payload)
-  if err != nil {
-    fmt.Printf("Failed to write payload to file: %v\n", err)
-    return
-  }
-  fmt.Printf("[+]Written %d bytes to %s\n", len(payload), filename)
+		n, err := ps.r.Read(tmp)
+		if n > 0 {
+			data := tmp[:n]
+      // or just write and let save append
+      saveFile(data,fileTypeDetected,&ps.fileLock)
+		}
+		// If EOF, exit loop
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Printf("[-] Error reading stream: %v\n", err)
+			return
+		}
+	}
 }
 
 func main() {
